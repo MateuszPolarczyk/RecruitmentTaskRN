0450e15f7800af913b41b07ec4e7dbf10fb5ffa1
MateuszPolarczyk <mateusz.polarczyk@interia.pl>
2025-04-27 21:59:25 +0200

    style: bottom tab bar styling

--- src/stacks/TabNavigation/index.tsx
+++ src/stacks/TabNavigation/index.tsx
@@ -1,20 +1,50 @@
 import React from 'react';
+import {View} from 'react-native';
 import {createBottomTabNavigator} from '@react-navigation/bottom-tabs';
 import {CharacterListScreen} from './screens/CharacterList';
 import {FavoriteCharactersScreen} from './screens/FavoriteCharacters';
+import {COLORS} from '../../theme/colors';
+
+import Ionicons from '@expo/vector-icons/Ionicons';
+
+type TabBarIconName = keyof typeof Ionicons.glyphMap;
 
 const Tab = createBottomTabNavigator();
 
 export const TabNavigationStack = () => {
   return (
-    <Tab.Navigator>
+    <Tab.Navigator
+      screenOptions={({route}) => ({
+        tabBarIcon: () => {
+          let iconName: TabBarIconName = 'person';
+
+          route.name === 'ALL CHARACTERS'
+            ? (iconName = 'person')
+            : (iconName = 'star');
+
+          return <Ionicons name={iconName} size={17} color={COLORS.white} />;
+        },
+        tabBarActiveBackgroundColor: COLORS.primaryGreen,
+        tabBarInactiveBackgroundColor: COLORS.darkGreen,
+        tabBarStyle: {
+          height: 70,
+        },
+        tabBarLabelStyle: {
+          color: COLORS.white,
+          fontSize: 14,
+          fontFamily: 'DM Mono',
+        },
+        tabBarIconStyle: {
+          marginTop: 5,
+        },
+      })}>
       <Tab.Screen
-        name="Characters"
+        name="ALL CHARACTERS"
         component={CharacterListScreen}
         options={{headerShown: false}}
       />
       <Tab.Screen
-        name="Favorites"
+        name="LIKED CHARACTERS"
         component={FavoriteCharactersScreen}
         options={{headerShown: false}}
       />
