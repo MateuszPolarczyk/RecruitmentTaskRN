0e05fd29a447f13fe99b80ac123a8b96faa78132
MateuszPolarczyk <mateusz.polarczyk@interia.pl>
2025-05-01 13:20:11 +0200

    feat: StatusBar usage

--- App.tsx
+++ App.tsx
@@ -1,5 +1,6 @@
 import {NavigationContainer} from '@react-navigation/native';
 import React from 'react';
+import {StatusBar} from 'react-native';
 
 import {MainStack} from './src/stacks/Main';
 
@@ -9,6 +10,7 @@ import {Navbar} from './src/components/Navbar/Navbar';
 function App(): React.JSX.Element {
   return (
     <SafeAreaView style={{flex: 1}}>
+      <StatusBar translucent />
       <NavigationContainer>
         <Navbar />
         <MainStack />
