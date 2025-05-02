6659122530077b9902e80cd52eada0b2b3d0a5da
MateuszPolarczyk <mateusz.polarczyk@interia.pl>
2025-05-02 14:14:46 +0200

    feat: react query wrapper

--- App.tsx
+++ App.tsx
@@ -1,3 +1,5 @@
+import {QueryClient, QueryClientProvider} from '@tanstack/react-query';
+
 import {NavigationContainer} from '@react-navigation/native';
 import React from 'react';
 import {StatusBar} from 'react-native';
@@ -7,15 +9,19 @@ import {MainStack} from './src/stacks/Main';
 import {SafeAreaView} from 'react-native-safe-area-context';
 import {Navbar} from './src/components/Navbar/Navbar';
 
+const queryClient = new QueryClient();
+
 function App(): React.JSX.Element {
   return (
-    <SafeAreaView style={{flex: 1}}>
-      <StatusBar translucent />
-      <NavigationContainer>
-        <Navbar />
-        <MainStack />
-      </NavigationContainer>
-    </SafeAreaView>
+    <QueryClientProvider client={queryClient}>
+      <SafeAreaView style={{flex: 1}}>
+        <StatusBar translucent />
+        <NavigationContainer>
+          <Navbar />
+          <MainStack />
+        </NavigationContainer>
+      </SafeAreaView>
+    </QueryClientProvider>
   );
 }
 
